f9ca5e1db111addf4fba2e07d425c3af69ad76ba
John Dias <jddias@gmail.com>
2025-09-06 15:44:31 -0500

    feat(ui): make Target Data Visualizer default (maps to RatioPlanner); hide charts during scan; add subframe input
    Default sidebar mode set to "Target Data Visualizer" and mapped to open the RatioPlanner page
    Charts no longer update while a scan is running
    Added subframe length (minutes) input and display of subs-needed in tooltips
    Minor backend scanner cleanup and tests added (parse/filter fixes)

--- astrosummary-ui/src/App.tsx
+++ astrosummary-ui/src/App.tsx
@@ -1,7 +1,7 @@
 import Sidebar from './components/Sidebar'
 import { useApp } from './context/AppContext'
 import AstroBinExport from './pages/AstroBinExport'
-import TargetDataVisualizer from './pages/TargetDataVisualizer'
+import RatioPlanner from './pages/RatioPlanner'
 
 export default function App() {
   const { mode } = useApp()
@@ -9,8 +9,8 @@ export default function App() {
     <div className="flex bg-bg-app text-text-primary min-h-screen">
       <Sidebar />
       <main className="flex-1 p-6 space-y-6">
-    {mode === 'AstroBin Export' && <AstroBinExport />}
-    {mode === 'Target Data Visualizer' && <TargetDataVisualizer />}
+  {mode === 'AstroBin Export' && <AstroBinExport />}
+  {mode === 'Target Data Visualizer' && <RatioPlanner />}
   {/* Target Filter Report moved into Target Data Visualizer view */}
       </main>
     </div>
